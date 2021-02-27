243f242b06d87a8139dde679a3f36fd2227814f0
Koni-Ts <tsantilak@gmail.com>
2021-02-27 18:20:39 +0000

    list of files in the directory with sub folders

--- script.py
+++ script.py
@@ -81,8 +81,29 @@ sec = LocalGetSections(courseid)
 
 import os
 
-print(os.listdir())
+#print(os.listdir())
 
-for root, dirs, files in os.walk("/workspace/CA3"):
-    for filename in files:
-        print(filename)
+os.listdir(path='/workspace/CA3')
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
+
+
+dirName = '/workspace/CA3';
+listOfFiles = getListOfFiles(dirName)
+
+print(listOfFiles)
